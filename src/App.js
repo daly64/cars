a60c3a5598d4db6a20e66e953d9be07e01a2ff08
dalyna64 <dalyna64@gmail.com>
2020-10-23 22:11:20 +0100

    episode 8

--- src/App.js
+++ src/App.js
@@ -5,10 +5,28 @@ import './App.css'
 
 export default class App extends Component {
   state = { title: "Mon Catalogue Voitures" };
+  changeTitle = (e) => {
+    this.setState({ title: "Mon Nouveau Titre" });
+  }
+
+  changerViaParam = (title) => {
+    this.setState({ title: title })
+  }
+  changerViaBind = (param) => {
+    this.setState({ title: param })
+  }
+  changerViaInput = (e) => {
+    this.setState({ title: e.target.value })
+  }
+
   render() {
     return (
       <div className="App">
       <MyCars title={this.state.title}/>
+        <button onClick={this.changeTitle}>changer le nom en dure</button>
+        <button onClick={() => this.changerViaParam("titre via un param")}>via param</button>
+        <button onClick={this.changerViaBind.bind(this, "titre via bind")} >via bind</button>
+        <input type="text" onChange={this.changerViaInput} value={this.state.title} />
     </div>
     );
   }
--- src/MyCars.js
+++ src/MyCars.js
@@ -2,11 +2,25 @@ import  React,{ Component } from 'react'
 import Car from './Cars';
 
 export default class MyCars extends Component {
+    noCopy = () => {
+        alert("merci de ne pas copier le text");
+    }
+/*     addStyle = (e) => {
+        let target = e.target.classList;
+        if (target.contains("styled")) {
+            target.remove("styled");
+        } else {
+            target.add("styled");
+        }
+    } */
+
     render() {
+        const { title } = this.props;
         return (
             <div>
-                <h1>{this.props.title} </h1>
+                <h1>{title}</h1>
 
+                <p onCopy={this.noCopy}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi molestias saepe velit nesciunt voluptas iusto officiis ratione. Veniam facere, blanditiis non quidem maiores mollitia vitae autem vel culpa odit? Maiores?</p>
                 <Car color='red'>ford</Car>
                 <Car >mercedes</Car>
                 <Car />
